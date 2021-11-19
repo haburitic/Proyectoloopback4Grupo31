65a04d32e20d243aad067f4536de96cb2f784e89
Hector <Hector>
2021-11-18 21:42:11 -0500

    Se adiciona map de contraseña

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -3,6 +3,12 @@ import {DefaultCrudRepository} from '@loopback/repository';
 import {MongoDataSource} from '../datasources';
 import {User, UserRelations} from '../models';
 
+export type Credentials = {
+  email: string;
+  password: string;
+  role?: string
+};
+
 export class UserRepository extends DefaultCrudRepository<
   User,
   typeof User.prototype.id,
